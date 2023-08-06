db4507516024767503ac3a87903545757ef13273
wu zhiyan <wu71418@gmail.com>
2023-08-06 16:25:04 +0800

    現在可以查詢 尚且不能新增到資料庫

--- product.php
+++ product.php
@@ -22,12 +22,19 @@
     
     
     
-    $sql="select * from 產品資料";
+   /* $sql="select * from 產品資料";
     $result=mysqli_query($link,$sql);
 
     while($row=mysqli_fetch_assoc($result)){
         echo $row['產品編號']."-".$row['產品名稱']."<br>";
-    }
-    mysqli_free_result($result);
+    }*/
+
+    $sql="insert into 產品資料(產品編號,類別編號,供應商編號,產品名稱,建議單價,平均成本,庫存量,安全存量) 
+    values($pd_num,$cat_num,$sup_num,$suggestion_price,$avg_cost,$inventory,$safe_amount)";
+    $result=mysqli_query($link,$sql);
+    $insert_id=mysqli_insert_id($link);
+    $new_id= mysqli_insert_id ($link);
+    echo "新增後的id為 {$new_id} ";
+
     mysqli_close($link);
 ?>
