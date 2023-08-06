4136d6e45ca66e4413b36574f5a61f9fff8fb08c
wu zhiyan <wu71418@gmail.com>
2023-08-06 18:58:27 +0800

    解決原本無法新增的問題(少了一欄) 要改進外來鍵被約束而無法新增的問題

--- product.php
+++ product.php
@@ -2,6 +2,7 @@
     $pd_num=$_POST['Pd_Num'];
     $cat_num=$_POST['Cat_Num'];
     $sup_num=$_POST['Sup_Num'];
+    $pd_name=$_POST['Pd_Name'];
     $suggestion_price=$_POST['Suggestion_Price'];
     $avg_cost=$_POST['Avg_Cost'];
     $inventory=$_POST['INVENTORY'];
@@ -30,11 +31,20 @@
     }*/
 
     $sql="insert into 產品資料(產品編號,類別編號,供應商編號,產品名稱,建議單價,平均成本,庫存量,安全存量) 
-    values($pd_num,$cat_num,$sup_num,$suggestion_price,$avg_cost,$inventory,$safe_amount)";
+    values($pd_num,$cat_num,$sup_num,$suggestion_price,$pd_name,$avg_cost,$inventory,$safe_amount)";
+
     $result=mysqli_query($link,$sql);
-    $insert_id=mysqli_insert_id($link);
+
+    if (mysqli_affected_rows($link)>0) {
     $new_id= mysqli_insert_id ($link);
     echo "新增後的id為 {$new_id} ";
+    }
+    elseif(mysqli_affected_rows($link)==0) {
+    echo "無資料新增";
+    }
+    else {
+    echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
+    }
 
     mysqli_close($link);
 ?>
